8a2673e73a2dce232b8dc3020969fba083501482
Yusers <tanyusisreal@gmail.com>
2023-05-19 14:06:46 +0700

    init

---
+++ index.js
@@ -0,0 +1,29 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const { Configuration, OpenAIApi } = require("openai");
+
+const config = new Configuration({
+  apiKey: "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",
+});
+const openai = new OpenAIApi(config);
+// setup Server
+const app = express();
+app.use(bodyParser.json());
+app.use(cors());
+
+app.post("/chat", async (req, res) => {
+  const { prompt } = req.body;
+  const completion = await openai.createChatCompletion({
+    model: "gpt-3.5-turbo",
+    max_tokens: 3000,
+    messages: [{ role: "user", content: prompt }],
+  });
+
+  res.send(completion.data.choices[0].message.content);
+});
+
+const port = 8080;
+app.listen(port, () => {
+  console.log(`Sever listening on port ${port}`);
+});
